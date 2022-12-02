699d6b27338a9537aeeadbbf736f1865fa7cd604
Aayush <>
2022-12-02 14:20:35 +0530

    Random Jokes

---
+++ script.js
@@ -0,0 +1,15 @@
+let bioData={
+    fName:'Aayush',
+    lname:'Mishra',
+    pNumber:9106720142,
+    Email:'ayush28014@gmail.com',
+    show(){
+        console.log(`My Name is ${bioData.fName} ${bioData.lname}`);
+        console.log(`and Phone Number is ${bioData.pNumber} with E-mail Id =${bioData.Email}`);
+    },
+};
+bioData.show();
+bioData()
+{
+    // please help me i am afraid of doing this please help me please
+}
